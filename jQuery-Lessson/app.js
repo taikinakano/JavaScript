52be9fef59808129fe4c1760e0f5f9a42864d775
中野太貴 <kabayaki3323@gmail.com>
2021-05-19 21:13:30 +0900

    [Finish]6-5

--- jQuery-Lessson/app.js
+++ jQuery-Lessson/app.js
@@ -1,3 +1,14 @@
+/*$(function() {
+    $('.box1').css({
+        'background-color': '#0000FF',
+        'height': '100px'
+    });*/
+//});
+$(function(){
+    $('.box1').hide();
+});
+
+
 /*$(document).ready(function () {
     $('body').html('<h1>Hello jQuery!!</h1>');
   });
