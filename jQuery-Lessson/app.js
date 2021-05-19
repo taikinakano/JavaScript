01cbca2d1d0526e2120b910e8f3c1bcec4f3f459
中野太貴 <kabayaki3323@gmail.com>
2021-05-19 21:34:03 +0900

    [Finish]6-6

--- jQuery-Lessson/app.js
+++ jQuery-Lessson/app.js
@@ -1,7 +1,20 @@
 
+
 $(function(){
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
+    });
+    $('.book1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
+    });
+});
+
+/*$(function(){
     $('.box1').mouseover(function(){
-        $('.box1').css({'background-color': '#0000FF'});
+        $('.box1').addClass('box1-ext');
+    });
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
     });
 });
 
